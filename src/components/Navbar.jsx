815d79268282856b8a75c94584486fa070cb2598
Gonza <gonzalogramagia@gmail.com>
2026-04-03 09:14:47 -0300

    fix(ui): update navbar links and fix ci node version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import { FaGithub } from 'react-icons/fa';
 
- 
+
 const Navbar = () => {
 
-  return (
-    <nav>
-        <a 
-            href="https://front.gonza.gr/" 
-            target="_blank"
-        >
-            <img src={logo} alt="logo" className='logo' /> 
-        </a>
-        <a 
-            href="https://github.com/gonzagramaglia/notes" 
-            className='github-logo-container'
-            target="_blank"
-        >
-            <FaGithub size={23} style={{ color: '#a9a6c3' }} />
-        </a>
-    </nav>
-  )
+    return (
+        <nav>
+            <a
+                href="https://gonzalogramagia.github.io/project-quick-notes"
+                target="_blank"
+            >
+                <img src={logo} alt="logo" className='logo' />
+            </a>
+            <a
+                href="https://github.com/gonzalogramagia/project-quick-notes"
+                className='github-logo-container'
+                target="_blank"
+            >
+                <FaGithub size={23} style={{ color: '#a9a6c3' }} />
+            </a>
+        </nav>
+    )
 }
 
 export default Navbar
